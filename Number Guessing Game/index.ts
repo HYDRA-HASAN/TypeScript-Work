2ad75d37f5c15360a5f87c4e1920421115efca22
Hassan Yaseen Siddiqui <hassanyaseen615@gmail.com>
2024-03-28 04:46:23 +0500

    new

---
+++ Number Guessing Game/index.ts
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+
+async function startFunc() {
+    const systemNum = Math.floor(Math.random() * 10);
+    const userNum = await inquirer.prompt([
+        {
+            type: "number",
+            name: "userGuess",
+            message: "Guess a number B/W 1 & 10: ",
+        }
+    ]);
+
+    const { userGuess } = userNum;
+
+    console.log("Your Guess: ", userGuess, "\nSystem's Guess: ", systemNum);
+    
+    if(userGuess === systemNum) {
+        console.log("Your Guess is Correct \nYou Won!");
+    }
+    else{
+        console.log("Your Guess is Incorrect \nYou Lost!");
+    }
+
+}
+
+async function startAgain(){
+    do{
+        await startFunc();
+        var again = await inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "restart",
+                message: "Do you want to play again? Y or N : ",
+            }
+        ])
+    }
+    while(again.restart === "Y" || again.restart === "y");
+}    
+
+startAgain();
+
