80b71491563ea9f1bab05ac83767bdb629f672dc
Hassan Yaseen Siddiqui <hassanyaseen615@gmail.com>
2024-03-29 22:09:51 +0500

    new

---
+++ Word Counter/index.ts
@@ -0,0 +1,19 @@
+import inquirer from 'inquirer';
+
+async function wordCounter() {
+    const { paragraph } = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'paragraph',
+            message: 'Enter a paragraph:',
+        },
+    ]);
+
+    const characterCount = paragraph.replace(/\s/g, '').length;
+    const wordCount = paragraph.trim().split(/\s+/).length;
+
+    console.log(`Character count: ${characterCount}`);
+    console.log(`Word count: ${wordCount}`);
+}
+
+wordCounter();
